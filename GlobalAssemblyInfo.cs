cdc4f102da984c2357a01c7cf882274579a5e706
Justin Rockwood <justinrockwood@hotmail.com>
2017-05-29 23:45:48 -0700

    Add GlobalAssemblyInfo with attributes applicable to all projects

---
+++ GlobalAssemblyInfo.cs
@@ -0,0 +1,36 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="GlobalAssemblyInfo.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("")]
+#endif
+
+// Product Information
+[assembly: AssemblyCompany("Justin Rockwood")]
+[assembly: AssemblyCopyright("Copyright © Justin Rockwood. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("Desalt Toolset")]
+
+// Globalization and Localization
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// COM and CLS Compliance
+[assembly: CLSCompliant(false)]
+[assembly: ComVisible(false)]
+
+// Version Information
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyInformationalVersion("0.1 Pre-Release")]
