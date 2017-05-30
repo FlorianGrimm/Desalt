d2385906c90e34d55cb4d8d8b002d3c373c245a4
Justin Rockwood <justinrockwood@hotmail.com>
2017-05-30 00:46:19 -0700

    Add Desalt.TypeScript project

---
+++ src/Desalt.TypeScript/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("Desalt.TypeScript")]
+[assembly: AssemblyDescription("TypeScript grammar and syntax.")]
