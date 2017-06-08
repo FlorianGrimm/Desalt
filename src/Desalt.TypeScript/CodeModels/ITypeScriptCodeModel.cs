636b64232e8e7d4346a2ddfcd41ef87444f91123
Justin Rockwood <justinrockwood@hotmail.com>
2017-06-07 21:10:47 -0700

    Add ITypeScriptCodeModel and TypeScriptCodeModel

---
+++ src/Desalt.TypeScript/CodeModels/ITypeScriptCodeModel.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="ITypeScriptCodeModel.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.TypeScript.CodeModels
+{
+    using Desalt.Core.CodeModels;
+
+    /// <summary>
+    /// Root interface for all TypeScript code models.
+    /// </summary>
+    public interface ITypeScriptCodeModel : ICodeModel
+    {
+        T Accept<T>(TypeScriptVisitor<T> visitor);
+    }
+}
