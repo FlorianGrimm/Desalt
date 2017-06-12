33f1c0cdc6fdc3745d7bb74dd94cf0c757ba67e3
Justin Rockwood <justinrockwood@hotmail.com>
2017-06-11 23:02:17 -0700

    Add Emitter and unit tests

---
+++ src/Desalt.Core/Emit/Emitter.cs
@@ -0,0 +1,125 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="Emitter.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.Core.Emit
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using Desalt.Core.Utility;
+
+    /// <summary>
+    /// General-purpose emitter for serializing code models into code.
+    /// </summary>
+    public class Emitter : IDisposable
+    {
+        //// ===========================================================================================================
+        //// Member Variables
+        //// ===========================================================================================================
+
+        public static readonly Encoding DefaultEncoding = new UTF8Encoding(
+            encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true);
+
+        private readonly StreamWriter _streamWriter;
+        private readonly IndentedTextWriter _writer;
+
+        //// ===========================================================================================================
+        //// Constructors
+        //// ===========================================================================================================
+
+        public Emitter(Stream outputStream, Encoding encoding = null, EmitOptions options = null)
+        {
+            if (outputStream == null)
+            {
+                throw new ArgumentNullException(nameof(outputStream));
+            }
+
+            Encoding = encoding ?? DefaultEncoding;
+            Options = options ?? EmitOptions.Default;
+
+            _streamWriter = new StreamWriter(outputStream, Encoding, bufferSize: 1024, leaveOpen: true)
+            {
+                AutoFlush = true
+            };
+            _writer = new IndentedTextWriter(_streamWriter, Options.IndentationPrefix)
+            {
+                NewLine = Options.Newline
+            };
+        }
+
+        //// ===========================================================================================================
+        //// Properties
+        //// ===========================================================================================================
+
+        public Encoding Encoding { get; }
+
+        public EmitOptions Options { get; }
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        public void Write(string text)
+        {
+            _writer.Write(text);
+        }
+
+        public void WriteBlock(Action writeBodyAction, bool isSimpleBlock = false)
+        {
+            _writer.Write("{");
+
+            bool indentBlock = (isSimpleBlock && Options.SimpleBlockOnNewLine) ||
+                (!isSimpleBlock && Options.NewlineAfterOpeningBrace);
+
+            if (indentBlock)
+            {
+                _writer.WriteLine();
+                _writer.IndentLevel++;
+            }
+            else if (Options.SpaceWithinSimpleBlockBraces)
+            {
+                _writer.Write(" ");
+            }
+
+            writeBodyAction();
+
+            if (indentBlock)
+            {
+                _writer.IndentLevel--;
+            }
+
+            // ReSharper disable ArrangeBraces_ifelse
+            if ((isSimpleBlock && Options.SimpleBlockOnNewLine) ||
+                (!isSimpleBlock && Options.NewlineBeforeClosingBrace))
+            {
+                _writer.WriteLine();
+            }
+            else if (isSimpleBlock && Options.SpaceWithinSimpleBlockBraces)
+            {
+                _writer.Write(" ");
+            }
+            // ReSharper restore ArrangeBraces_ifelse
+
+            _writer.Write("}");
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _writer.Dispose();
+                _streamWriter.Dispose();
+            }
+        }
+    }
+}
---
+++ test/Desalt.Core.Tests/Emit/EmitterTests.cs
@@ -0,0 +1,116 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="EmitterTests.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.Core.Tests.Emit
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using Desalt.Core.Emit;
+    using Desalt.Core.Extensions;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class EmitterTests
+    {
+        private static readonly EmitOptions s_testOptions = EmitOptions.Default
+            .WithNewline("\n")
+            .WithIndentationPrefix("\t");
+
+        [TestMethod]
+        public void Ctor_should_throw_on_null_args()
+        {
+            // ReSharper disable ObjectCreationAsStatement
+            Action action = () => new Emitter(outputStream: null);
+            action.ShouldThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("outputStream");
+            // ReSharper restore ObjectCreationAsStatement
+        }
+
+        [TestMethod]
+        public void Ctor_should_store_the_encoding_parameter_in_the_property()
+        {
+            var emitter = new Emitter(new MemoryStream(), Encoding.ASCII);
+            emitter.Encoding.Should().BeSameAs(Encoding.ASCII);
+        }
+
+        [TestMethod]
+        public void Ctor_should_use_UTF8_no_BOM_encoding_if_not_supplied()
+        {
+            var emitter = new Emitter(new MemoryStream());
+            emitter.Encoding.EncodingName.Should().Be(Encoding.UTF8.EncodingName);
+            emitter.Encoding.GetPreamble().Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void Ctor_should_store_the_options_parameter_in_the_property()
+        {
+            var options = EmitOptions.Default.WithIndentationPrefix("\v");
+            var emitter = new Emitter(new MemoryStream(), options: options);
+            emitter.Options.Should().BeSameAs(options);
+        }
+
+        [TestMethod]
+        public void WriteBlock_should_surround_the_body_with_braces()
+        {
+            using (var stream = new MemoryStream())
+            using (var writer = new StreamWriter(stream) { AutoFlush = true })
+            {
+                var emitter = new Emitter(stream, options: s_testOptions);
+                // ReSharper disable once AccessToDisposedClosure
+                emitter.WriteBlock(() => writer.Write("body"));
+                stream.ReadAllText().Should().Be("{\nbody\n}");
+            }
+        }
+
+        [TestMethod]
+        public void WriteBlock_should_indent_the_contents_if_necessary()
+        {
+            using (var stream = new MemoryStream())
+            {
+                var emitter = new Emitter(stream, options: s_testOptions);
+                emitter.WriteBlock(() => emitter.Write("text"));
+                stream.ReadAllText().Should().Be("{\n\ttext\n}");
+            }
+        }
+
+        [TestMethod]
+        public void WriteBlock_should_use_the_options_for_formatting_simple_blocks()
+        {
+            using (var stream = new MemoryStream())
+            {
+                var emitter = new Emitter(stream, options: s_testOptions.WithSimpleBlockOnNewLine(false));
+                emitter.WriteBlock(() => emitter.Write("text"), isSimpleBlock: true);
+                stream.ReadAllText().Should().Be("{ text }");
+            }
+
+            using (var stream = new MemoryStream())
+            {
+                var emitter = new Emitter(stream, options: s_testOptions.WithSimpleBlockOnNewLine(true));
+                emitter.WriteBlock(() => emitter.Write("text"), isSimpleBlock: true);
+                stream.ReadAllText().Should().Be("{\n\ttext\n}");
+            }
+        }
+
+        [TestMethod]
+        public void WriteBlock_should_use_the_options_for_formatting_non_simple_blocks()
+        {
+            var options = s_testOptions
+                .WithNewlineBeforeClosingBrace(false)
+                .WithNewlineAfterOpeningBrace(false)
+                .WithSpaceWithinSimpleBlockBraces(false);
+
+            using (var stream = new MemoryStream())
+            using (var emitter = new Emitter(stream, options: options))
+            {
+                // ReSharper disable once AccessToDisposedClosure
+                emitter.WriteBlock(() => emitter.Write("text"));
+                stream.ReadAllText().Should().Be("{text}");
+            }
+        }
+    }
+}
