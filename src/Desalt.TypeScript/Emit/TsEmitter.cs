df7244a50cabad9c7af004e7829e48c3e641d037
Justin Rockwood <justinrockwood@hotmail.com>
2017-06-13 19:20:40 -0700

    Add TypeScriptEmitter and unit tests

---
+++ src/Desalt.TypeScript/Emit/TsEmitter.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="TsEmitter.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.TypeScript.Emit
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using Desalt.Core.Emit;
+    using Desalt.TypeScript.Ast;
+
+    /// <summary>
+    /// Takes an <see cref="ITsAstNode"/> and converts it to text.
+    /// </summary>
+    public class TsEmitter : TsVisitor, IDisposable
+    {
+        //// ===========================================================================================================
+        //// Member Variables
+        //// ===========================================================================================================
+
+        private readonly Emitter<ITsAstNode> _emitter;
+        private readonly EmitOptions _options;
+
+        //// ===========================================================================================================
+        //// Constructors
+        //// ===========================================================================================================
+
+        public TsEmitter(Stream outputStream, Encoding encoding = null, EmitOptions options = null)
+        {
+            _emitter = new Emitter<ITsAstNode>(outputStream, encoding, options);
+            _options = options;
+        }
+
+        //// ===========================================================================================================
+        //// Properties
+        //// ===========================================================================================================
+
+        public Encoding Encoding => _emitter.Encoding;
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        public void Dispose()
+        {
+            _emitter.Dispose();
+        }
+    }
+}
---
+++ test/Desalt.TypeScript.Tests/Emit/TsEmitterTests.cs
@@ -0,0 +1,40 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="TsEmitterTests.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.TypeScript.Tests.Emit
+{
+    using System;
+    using System.IO;
+    using Desalt.Core.Emit;
+    using Desalt.Core.Extensions;
+    using Desalt.TypeScript.Ast;
+    using Desalt.TypeScript.Emit;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public partial class TsEmitterTests
+    {
+        private static void VerifyOutput(ITsAstNode model, string expected, EmitOptions options = null)
+        {
+            using (var stream = new MemoryStream())
+            using (var emitter = new TsEmitter(stream, options: options ?? EmitOptions.Default))
+            {
+                emitter.Visit(model);
+                stream.ReadAllText(emitter.Encoding).Should().Be(expected);
+            }
+        }
+
+        [TestMethod]
+        public void Ctor_should_throw_on_null_args()
+        {
+            // ReSharper disable once ObjectCreationAsStatement
+            Action action = () => new TsEmitter(outputStream: null);
+            action.ShouldThrowExactly<ArgumentNullException>().And.ParamName.Should().Be("outputStream");
+        }
+    }
+}
