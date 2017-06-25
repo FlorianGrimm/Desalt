37766dd3fea6f1dc44157951c5efc720f9135af2
Justin Rockwood <justinrockwood@hotmail.com>
2017-06-24 19:25:13 -0700

    Rename TypeScriptGrammarInterfaces to TsGrammarInterfaces

--- src/Desalt.TypeScript/Ast/TsGrammarInterfaces.cs
+++ src/Desalt.TypeScript/Ast/TsGrammarInterfaces.cs
@@ -1,5 +1,5 @@
 ﻿// ---------------------------------------------------------------------------------------------------------------------
-// <copyright file="TypeScriptGrammar.cs" company="Justin Rockwood">
+// <copyright file="TsGrammarInterfaces.cs" company="Justin Rockwood">
 //   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
 //   LICENSE.txt in the project root for license information.
 // </copyright>
