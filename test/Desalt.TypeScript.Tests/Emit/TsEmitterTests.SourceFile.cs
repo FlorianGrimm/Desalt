b0536f6ff0c614a678b80e4e21721bd491865ba9
Justin Rockwood <justinrockwood@hotmail.com>
2017-06-24 18:38:34 -0700

    Add placeholder test file for ImplementationScript

---
+++ test/Desalt.TypeScript.Tests/Emit/TsEmitterTests.SourceFile.cs
@@ -0,0 +1,20 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="TypeScriptEmitterTests.SourceFile.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.TypeScript.Tests.Emit
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    public partial class TsEmitterTests
+    {
+        [TestMethod]
+        public void Emit_an_ImplementationScript()
+        {
+            Assert.Inconclusive("Not implemented yet");
+        }
+    }
+}
