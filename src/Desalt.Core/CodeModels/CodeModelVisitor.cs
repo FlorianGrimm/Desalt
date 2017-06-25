1ee3e537cf8a49603da16b3497793f853e7aa8b7
Justin Rockwood <justinrockwood@hotmail.com>
2017-06-24 18:52:55 -0700

    Rename ICodeModelVisitor to IAstVisitor

--- src/Desalt.Core/CodeModels/CodeModelVisitor.cs
+++ src/Desalt.Core/CodeModels/CodeModelVisitor.cs
@@ -10,36 +10,36 @@ namespace Desalt.Core.CodeModels
     using System;
 
     /// <summary>
-    /// Abstract base class for an <see cref="ICodeModelVisitor{TModel, TResult}"/> visitor that
-    /// visits only the single model passed into its Visit method.
+    /// Abstract base class for an <see cref="IAstVisitor{TModel}"/> visitor that
+    /// visits only the single node passed into its Visit method.
     /// </summary>
-    /// <typeparam name="TModel">The type of the model to visit.</typeparam>
-    public abstract class CodeModelVisitor<TModel> : ICodeModelVisitor<TModel>
+    /// <typeparam name="TModel">The type of the node to visit.</typeparam>
+    public abstract class CodeModelVisitor<TModel> : IAstVisitor<TModel>
         where TModel : IAstNode
     {
         public abstract void Visit(TModel model);
 
-        public virtual void DefaultVisit(TModel model)
+        public virtual void DefaultVisit(TModel node)
         {
-            throw new InvalidOperationException($"{GetType().Name}: Model not supported: {model.GetType().Name}");
+            throw new InvalidOperationException($"{GetType().Name}: Model not supported: {node.GetType().Name}");
         }
     }
 
     /// <summary>
-    /// Abstract base class for an <see cref="ICodeModelVisitor{TModel, TResult}"/> visitor that
-    /// visits only the single model passed into its Visit method and produces a value of the type
+    /// Abstract base class for an <see cref="IAstVisitor{TModel}"/> visitor that
+    /// visits only the single node passed into its Visit method and produces a value of the type
     /// specified by the <typeparamref name="TResult"/> parameter.
     /// </summary>
-    /// <typeparam name="TModel">The type of the model to visit.</typeparam>
+    /// <typeparam name="TModel">The type of the node to visit.</typeparam>
     /// <typeparam name="TResult">The type of the return value this visitor's Visit method.</typeparam>
-    public abstract class CodeModelVisitor<TModel, TResult> : ICodeModelVisitor<TModel, TResult>
+    public abstract class CodeModelVisitor<TModel, TResult> : IAstVisitor<TModel, TResult>
         where TModel : IAstNode
     {
         public abstract TResult Visit(TModel model);
 
-        public virtual TResult DefaultVisit(TModel model)
+        public virtual TResult DefaultVisit(TModel node)
         {
-            throw new InvalidOperationException($"{GetType().Name}: Model not supported: {model.GetType().Name}");
+            throw new InvalidOperationException($"{GetType().Name}: Model not supported: {node.GetType().Name}");
         }
     }
 }
---
+++ src/Desalt.Core/CodeModels/IAstVisitor.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="IAstVisitor.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.Core.CodeModels
+{
+    /// <summary>
+    /// Service contract for a class that visits nodes in an abstract syntax tree (AST).
+    /// </summary>
+    /// <typeparam name="TNode">The type of nodes in the tree.</typeparam>
+    public interface IAstVisitor<in TNode> where TNode : IAstNode
+    {
+        void DefaultVisit(TNode node);
+
+        void Visit(TNode model);
+    }
+
+    /// <summary>
+    /// Service contract for a class that visits nodes in an abstract syntax tree (AST) and returns a
+    /// result after visiting the node.
+    /// </summary>
+    /// <typeparam name="TNode">The type of nodes in the tree.</typeparam>
+    /// <typeparam name="TResult">The type of the result after visiting the node.</typeparam>
+    public interface IAstVisitor<in TNode, out TResult> where TNode : IAstNode
+    {
+        TResult DefaultVisit(TNode node);
+
+        TResult Visit(TNode model);
+    }
+}
