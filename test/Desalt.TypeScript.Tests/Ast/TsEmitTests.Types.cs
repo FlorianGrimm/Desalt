bc56616fa23d3c5d756522a570dd0afc39e7fba6
Justin Rockwood <justinrockwood@hotmail.com>
2017-07-04 23:51:18 -0700

    Add test for TsTypeParameter

---
+++ test/Desalt.TypeScript.Tests/Ast/TsEmitTests.Types.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="TsEmitTests.Types.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.TypeScript.Tests.Ast
+{
+    using Desalt.TypeScript.Ast;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    public partial class TsEmitTests
+    {
+        [TestMethod]
+        public void Emit_type_parameter_with_no_constraint()
+        {
+            VerifyOutput(TsAstFactory.TypeParameter(s_T), "T");
+        }
+
+        [TestMethod]
+        public void Emit_type_parameter_with_constraint()
+        {
+            VerifyOutput(TsAstFactory.TypeParameter(s_T, TsAstFactory.TypeReference(s_MyType)), "T extends MyType");
+        }
+    }
+}
