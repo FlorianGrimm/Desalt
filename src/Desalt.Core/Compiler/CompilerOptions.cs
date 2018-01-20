4318a70ce6c520fa35bc4d1505ddfff5ea088cbb
Justin Rockwood <justinrockwood@hotmail.com>
2018-01-19 17:33:35 -0800

    feat: start of the compiler stages

---
+++ src/Desalt.Core/Compiler/CompilerOptions.cs
@@ -0,0 +1,50 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="CompilerOptions.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.Core.Compiler
+{
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+
+    /// <summary>
+    /// Contains options that control how to compile C# into TypeScript.
+    /// </summary>
+    public class CompilerOptions
+    {
+        //// ===========================================================================================================
+        //// Constructors
+        //// ===========================================================================================================
+
+        /// <summary>
+        /// Default constructor contains the default values of all of the options.
+        /// </summary>
+        public CompilerOptions(int warningLevel = 4)
+        {
+            WarningLevel = warningLevel;
+        }
+
+        //// ===========================================================================================================
+        //// Properties
+        //// ===========================================================================================================
+
+        /// <summary>
+        /// Gets the global warning level (from 0 to 4).
+        /// </summary>
+        public int WarningLevel { get; }
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        internal CSharpCompilationOptions ToCSharpCompilationOptions()
+        {
+            return new CSharpCompilationOptions(
+                OutputKind.DynamicallyLinkedLibrary,
+                warningLevel: WarningLevel);
+        }
+    }
+}
---
+++ src/Desalt.Core/Compiler/OpenProjectStage.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="OpenProjectStage.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.Core.Compiler
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Desalt.Core.Pipeline;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.MSBuild;
+
+    /// <summary>
+    /// Pipeline stage that opens a .csproj file using Roslyn.
+    /// </summary>
+    internal class OpenProjectStage : PipelineStage<CompilationRequest, Project>
+    {
+        public override async Task<IExtendedResult<Project>> ExecuteAsync(
+            CompilationRequest input,
+            CancellationToken cancellationToken = new CancellationToken())
+        {
+            // try to open the project
+            MSBuildWorkspace workspace = MSBuildWorkspace.Create();
+            Project project = await workspace.OpenProjectAsync(input.ProjectFilePath, cancellationToken);
+
+            return new ExtendedResult<Project>(project, new List<DiagnosticMessage>());
+        }
+    }
+}
