07391ec12b94bf23febd1c0a594b2af2433d0157
Justin Rockwood <justinrockwood@hotmail.com>
2018-02-15 00:44:01 -0800

    feat: add DiagnosticExtensions.NoErrors and NoErrorsOrWarnings methods

--- src/Desalt.Core/Extensions/DiagnosticExtensions.cs
+++ src/Desalt.Core/Extensions/DiagnosticExtensions.cs
@@ -19,8 +19,8 @@ namespace Desalt.Core.Extensions
         /// <summary>
         /// Converts all of the specified non-hidden diagnostics to a list of <see cref="DiagnosticMessage"/>.
         /// </summary>
-        /// <param name="diagnostics"></param>
-        /// <returns></returns>
+        /// <param name="diagnostics">A list of Roslyn <see cref="Diagnostic"/> messages.</param>
+        /// <returns>A converted list of <see cref="DiagnosticMessage"/> messages.</returns>
         public static IEnumerable<DiagnosticMessage> ToDiagnosticMessages(this IEnumerable<Diagnostic> diagnostics)
         {
             diagnostics = diagnostics ?? Enumerable.Empty<Diagnostic>();
@@ -29,6 +29,31 @@ namespace Desalt.Core.Extensions
             return diagnostics
                 .Where(d => d.Severity != DiagnosticSeverity.Hidden)
                 .Select(DiagnosticMessage.FromDiagnostic);
+        }
+
+        /// <summary>
+        /// Returns a value indicating whether none of the messages are errors.
+        /// </summary>
+        /// <param name="diagnostics">A list of <see cref="DiagnosticMessage"/> messages.</param>
+        /// <returns>
+        /// true if none of the messages are errors; false if there is at least one error.
+        /// </returns>
+        public static bool NoErrors(this IEnumerable<DiagnosticMessage> diagnostics)
+        {
+            return diagnostics.All(diagnostic => !diagnostic.IsError);
+        }
+
+        /// <summary>
+        /// Returns a value indicating whether none of the messages are errors or warnings.
+        /// </summary>
+        /// <param name="diagnostics">A list of <see cref="DiagnosticMessage"/> messages.</param>
+        /// <returns>
+        /// true if none of the messages are errors or warnings; false if there is at least one error
+        /// or warning.
+        /// </returns>
+        public static bool NoErrorsOrWarnings(this IEnumerable<DiagnosticMessage> diagnostics)
+        {
+            return diagnostics.All(diagnostic => !diagnostic.IsError && !diagnostic.IsWarning);
         }
     }
 }
