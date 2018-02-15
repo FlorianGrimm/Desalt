7f5a606c08adbb5cc0c4b4e09917938e77db93ed
Justin Rockwood <justinrockwood@hotmail.com>
2018-02-15 00:48:46 -0800

    feat: start of compiling interface declarations
    Just the name is compiled right now, but this is a great start.

--- src/Desalt.Core/Compiler/CompileProjectToTypeScriptStage.cs
+++ src/Desalt.Core/Compiler/CompileProjectToTypeScriptStage.cs
@@ -7,11 +7,18 @@
 
 namespace Desalt.Core.Compiler
 {
+    using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
+    using Desalt.Core.Emit;
+    using Desalt.Core.Extensions;
     using Desalt.Core.Pipeline;
+    using Desalt.Core.Translation;
+    using Desalt.Core.TypeScript.Ast;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
 
     /// <summary>
     /// Pipeline stage that compiles all of the C# files in a .csproj file to TypeScript.
@@ -33,20 +40,59 @@ namespace Desalt.Core.Compiler
             CancellationToken cancellationToken = default(CancellationToken))
         {
             Directory.CreateDirectory(options.OutputPath);
+            IEnumerable<Task<IExtendedResult<bool>>> tasks = input.Documents
+                .Where(document => document.Name == "ILogAppender.cs")
+                .Select(document => CompileToTypeScript(document, options, cancellationToken));
+            IExtendedResult<bool>[] results = await Task.WhenAll(tasks);
 
-            foreach (Document document in input.Documents)
+            return new ExtendedResult<bool>(
+                results.All(result => result.Result),
+                results.SelectMany(result => result.Messages));
+        }
+
+        private async Task<IExtendedResult<bool>> CompileToTypeScript(
+            Document document,
+            CompilerOptions options,
+            CancellationToken cancellationToken)
+        {
+            string typeScriptFilePath = Path.Combine(
+                options.OutputPath,
+                Path.GetFileNameWithoutExtension(document.FilePath) + ".ts");
+
+            var diagnostics = new List<DiagnosticMessage>();
+
+            if (!(await document.GetSyntaxTreeAsync(cancellationToken) is CSharpSyntaxTree syntaxTree))
             {
-                string typeScriptFilePath = Path.Combine(
-                    options.OutputPath,
-                    Path.GetFileNameWithoutExtension(document.FilePath) + ".ts");
-
-                using (var writer = new StreamWriter(typeScriptFilePath))
-                {
-                    await writer.WriteLineAsync($"// This is {typeScriptFilePath}");
-                }
+                return new ExtendedResult<bool>(
+                    true,
+                    new[] { DiagnosticMessage.Info($"File does not contain a syntax tree: {document.FilePath}") });
             }
 
-            return new ExtendedResult<bool>(true);
+            diagnostics.AddRange(syntaxTree.GetDiagnostics().ToDiagnosticMessages());
+
+            // TEMP - copy the original .cs file for easy comparing
+            // ReSharper disable once AssignNullToNotNullAttribute
+            File.Copy(document.FilePath, Path.Combine(options.OutputPath, Path.GetFileName(document.FilePath)));
+
+            // translate the C# syntax tree to TypeScript
+            var translator = new CSharpToTypeScriptTranslator();
+            IExtendedResult<ITsImplementationSourceFile> translation =
+                await translator.TranslateSyntaxTreeAsync(syntaxTree, cancellationToken);
+            diagnostics.AddRange(translation.Messages);
+
+            using (var stream = new FileStream(
+                typeScriptFilePath,
+                FileMode.Create,
+                FileAccess.ReadWrite,
+                FileShare.Read))
+            using (var emitter = new Emitter(stream))
+            {
+                ITsImplementationSourceFile typeScriptImplementationFile = translation.Result;
+                typeScriptImplementationFile?.Emit(emitter);
+            }
+
+            bool successful = options.WarningsAsErrors ? diagnostics.NoErrorsOrWarnings() : diagnostics.NoErrors();
+            return new ExtendedResult<bool>(successful, diagnostics);
         }
     }
 }
