868489d061ba57871dafdc7620689ea9aa5559e1
Justin Rockwood <justinrockwood@hotmail.com>
2018-03-29 01:50:37 -0700

    fix: leave off return type for arrow functions

---
+++ test/Desalt.Core.Tests/Translation/TranslationVisitorTests.Statements.cs
@@ -0,0 +1,39 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="TranslationVisitor.Tests.Statements.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.Core.Tests.Translation
+{
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    public partial class TranslationVisitorTests
+    {
+        [TestMethod]
+        public async Task Translate_anonymous_methods_should_correctly_infer_the_return_type()
+        {
+            await AssertTranslation(
+                @"
+using System;
+
+class C {
+    void Method() {
+        Func<string, boolean> func = delegate(string x) {
+            return x == ""y"";
+        };
+    }
+}",
+                @"class C {
+  private method(): void {
+    let func: (string: string) => boolean = (x: string) => {
+      return x === 'y';
+    };
+  }
+}
+");
+        }
+    }
+}
