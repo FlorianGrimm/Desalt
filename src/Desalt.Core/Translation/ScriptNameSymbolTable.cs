e7ba94872d7d5c85c1bb22e815fe581d50243c5c
Justin Rockwood <justinrockwood@hotmail.com>
2018-03-28 21:56:00 -0700

    feat: make ScriptNameSymbolTable more robust

--- src/Desalt.Core/Translation/ScriptNameSymbolTable.cs
+++ src/Desalt.Core/Translation/ScriptNameSymbolTable.cs
@@ -7,11 +7,10 @@
 
 namespace Desalt.Core.Translation
 {
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading;
     using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.CSharp;
-    using Microsoft.CodeAnalysis.CSharp.Syntax;
 
     /// <summary>
     /// Contains mappings of defined types in a C# project to the translated names (what they will be
@@ -32,21 +31,50 @@ namespace Desalt.Core.Translation
             DocumentTranslationContext context,
             CancellationToken cancellationToken)
         {
-            var allTypeDeclarations = context.RootSyntax.DescendantNodes().OfType<BaseTypeDeclarationSyntax>();
-            foreach (BaseTypeDeclarationSyntax node in allTypeDeclarations)
+            IEnumerable<INamedTypeSymbol> allTypeDeclarationSymbols = context.RootSyntax
+                .GetAllDeclaredTypes(context.SemanticModel, cancellationToken)
+                .Where(symbol => symbol.TypeKind != TypeKind.Delegate);
+
+            foreach (INamedTypeSymbol symbol in allTypeDeclarationSymbols)
             {
-                INamedTypeSymbol symbol = context.SemanticModel.GetDeclaredSymbol(node, cancellationToken);
                 string typeName = symbol.Name;
                 AddOrUpdate(symbol, typeName);
 
-                // add all of the members of the declared type
-                foreach (ISymbol member in symbol.GetMembers())
+                // add all of the members of the declared type, but skip over compiler-generated
+                // stuff like auto-property backing fields, event add/remove functions, and property
+                // get/set methods.
+                foreach (ISymbol member in symbol.GetMembers().Where(sym => !sym.IsImplicitlyDeclared))
                 {
                     string memberName = member.Name;
-                    string scriptName = char.ToLowerInvariant(memberName[0]) + memberName.Substring(1);
+                    string scriptName = ToCamelCase(memberName);
+
+                    if (member is IMethodSymbol methodMember)
+                    {
+                        // ReSharper disable once SwitchStatementMissingSomeCases
+                        switch (methodMember.MethodKind)
+                        {
+                            // skip constructors
+                            case MethodKind.Constructor:
+                            case MethodKind.StaticConstructor:
+                                continue;
+
+                            // skip property get/set methods
+                            case MethodKind.PropertyGet:
+                            case MethodKind.PropertySet:
+                                continue;
+
+                            // skip event add/remove
+                            case MethodKind.EventAdd:
+                            case MethodKind.EventRemove:
+                                continue;
+                        }
+                    }
+
                     AddOrUpdate(member, scriptName);
                 }
             }
         }
+
+        private static string ToCamelCase(string name) => char.ToLowerInvariant(name[0]) + name.Substring(1);
     }
 }
---
+++ test/Desalt.Core.Tests/Translation/ScriptNameSymbolTableTests.cs
@@ -0,0 +1,88 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="ScriptNameSymbolTableTests.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.Core.Tests.Translation
+{
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Desalt.Core.Tests.TestUtility;
+    using Desalt.Core.Translation;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ScriptNameSymbolTableTests
+    {
+        private static async Task AssertEntriesInSymbolTable(
+            string code,
+            params KeyValuePair<string, string>[] expectedEntries)
+        {
+            using (var tempProject = TempProject.Create("TempProject", new TempProjectFile("File.cs", code)))
+            {
+                DocumentTranslationContext context = await tempProject.CreateContextForFileAsync("File.cs");
+
+                var symbolTable = new ScriptNameSymbolTable();
+                symbolTable.AddDefinedTypesInDocument(context, CancellationToken.None);
+
+                symbolTable.Should().BeEquivalentTo(expectedEntries);
+            }
+        }
+
+        [TestMethod]
+        public async Task ScriptNameSymbolTable_should_preserve_the_case_of_interfaces_classes_and_structs()
+        {
+            await AssertEntriesInSymbolTable(
+                "interface MyInterface {} class MyClass {} struct MyStruct {}",
+                new KeyValuePair<string, string>("interface MyInterface", "MyInterface"),
+                new KeyValuePair<string, string>("class MyClass", "MyClass"),
+                new KeyValuePair<string, string>("struct MyStruct", "MyStruct"));
+        }
+
+        [TestMethod]
+        public async Task ScriptNameSymbolTable_should_skip_instance_and_static_constructors()
+        {
+            await AssertEntriesInSymbolTable(
+                "class C { C(int x) { } static C() { }",
+                new KeyValuePair<string, string>("class C", "C"));
+        }
+
+        [TestMethod]
+        public async Task ScriptNameSymbolTable_should_not_store_delegate_types()
+        {
+            await AssertEntriesInSymbolTable("delegate void MyDelegate();");
+        }
+
+        [TestMethod]
+        public async Task ScriptNameSymbolTable_should_make_members_camelCase_by_default()
+        {
+            await AssertEntriesInSymbolTable(
+                "class C { int MyInt; void MyMethod() {} }",
+                new KeyValuePair<string, string>("class C", "C"),
+                new KeyValuePair<string, string>("C.MyInt", "myInt"),
+                new KeyValuePair<string, string>("C.MyMethod", "myMethod"));
+        }
+
+        [TestMethod]
+        public async Task ScriptNameSymbolTable_should_handle_events_but_no_add_remove_methods()
+        {
+            await AssertEntriesInSymbolTable(
+                "class C { event System.Action MyEvent; }",
+                new KeyValuePair<string, string>("class C", "C"),
+                new KeyValuePair<string, string>("event C.MyEvent", "myEvent"));
+        }
+
+        [TestMethod]
+        public async Task ScriptNameSymbolTable_should_handle_properties_but_no_get_set_methods()
+        {
+            await AssertEntriesInSymbolTable(
+                "class C { bool MyProperty { get; set; } }",
+                new KeyValuePair<string, string>("class C", "C"),
+                new KeyValuePair<string, string>("C.MyProperty", "myProperty"));
+        }
+    }
+}
