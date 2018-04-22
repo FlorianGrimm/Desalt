df46df1d404114ab386459928c98a36500514c8d
Justin Rockwood <justinrockwood@hotmail.com>
2018-04-21 22:05:23 -0700

    refactor: move SymbolTableUtils to its own file

--- src/Desalt.Core/Translation/SymbolTable.cs
+++ src/Desalt.Core/Translation/SymbolTable.cs
@@ -16,39 +16,6 @@ namespace Desalt.Core.Translation
     using Microsoft.CodeAnalysis;
 
     /// <summary>
-    /// Contains static utility methods for using a symbol table. Pulled out into a separate class
-    /// because statics aren't shared between generic instances.
-    /// </summary>
-    internal static class SymbolTableUtils
-    {
-        public static readonly IEqualityComparer<ISymbol> KeyComparer = new KeyEqualityComparer();
-
-        private static readonly SymbolDisplayFormat s_symbolDisplayFormat = new SymbolDisplayFormat(
-            SymbolDisplayGlobalNamespaceStyle.OmittedAsContaining,
-            SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces,
-            SymbolDisplayGenericsOptions.IncludeTypeParameters,
-            SymbolDisplayMemberOptions.IncludeContainingType | SymbolDisplayMemberOptions.IncludeParameters,
-            SymbolDisplayDelegateStyle.NameOnly,
-            SymbolDisplayExtensionMethodStyle.StaticMethod,
-            SymbolDisplayParameterOptions.IncludeName | SymbolDisplayParameterOptions.IncludeType,
-            SymbolDisplayPropertyStyle.NameOnly,
-            SymbolDisplayLocalOptions.IncludeType,
-            SymbolDisplayKindOptions.IncludeNamespaceKeyword |
-            SymbolDisplayKindOptions.IncludeTypeKeyword |
-            SymbolDisplayKindOptions.IncludeMemberKeyword,
-            SymbolDisplayMiscellaneousOptions.UseSpecialTypes);
-
-        public static string KeyFromSymbol(ISymbol symbol) => symbol.ToDisplayString(s_symbolDisplayFormat);
-
-        private sealed class KeyEqualityComparer : IEqualityComparer<ISymbol>
-        {
-            public bool Equals(ISymbol x, ISymbol y) => KeyFromSymbol(x).Equals(KeyFromSymbol(y));
-
-            public int GetHashCode(ISymbol obj) => KeyFromSymbol(obj).GetHashCode();
-        }
-    }
-
-    /// <summary>
     /// Interface for a symbol table that holds different information.
     /// </summary>
     internal interface IConcurrentSymbolTable
---
+++ src/Desalt.Core/Translation/SymbolTableUtils.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="SymbolTableUtils.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.Core.Translation
+{
+    using System.Collections.Generic;
+    using Microsoft.CodeAnalysis;
+
+    /// <summary>
+    /// Contains static utility methods for using a symbol table. Pulled out into a separate class
+    /// because statics aren't shared between generic instances.
+    /// </summary>
+    internal static class SymbolTableUtils
+    {
+        public static readonly IEqualityComparer<ISymbol> KeyComparer = new KeyEqualityComparer();
+
+        private static readonly SymbolDisplayFormat s_symbolDisplayFormat = new SymbolDisplayFormat(
+            SymbolDisplayGlobalNamespaceStyle.OmittedAsContaining,
+            SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces,
+            SymbolDisplayGenericsOptions.IncludeTypeParameters,
+            SymbolDisplayMemberOptions.IncludeContainingType | SymbolDisplayMemberOptions.IncludeParameters,
+            SymbolDisplayDelegateStyle.NameOnly,
+            SymbolDisplayExtensionMethodStyle.StaticMethod,
+            SymbolDisplayParameterOptions.IncludeName | SymbolDisplayParameterOptions.IncludeType,
+            SymbolDisplayPropertyStyle.NameOnly,
+            SymbolDisplayLocalOptions.IncludeType,
+            SymbolDisplayKindOptions.IncludeNamespaceKeyword |
+            SymbolDisplayKindOptions.IncludeTypeKeyword |
+            SymbolDisplayKindOptions.IncludeMemberKeyword,
+            SymbolDisplayMiscellaneousOptions.UseSpecialTypes);
+
+        public static string KeyFromSymbol(ISymbol symbol) => symbol.ToDisplayString(s_symbolDisplayFormat);
+
+        private sealed class KeyEqualityComparer : IEqualityComparer<ISymbol>
+        {
+            public bool Equals(ISymbol x, ISymbol y) => KeyFromSymbol(x).Equals(KeyFromSymbol(y));
+
+            public int GetHashCode(ISymbol obj) => KeyFromSymbol(obj).GetHashCode();
+        }
+    }
+}
