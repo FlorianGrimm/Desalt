1a971140bee7892bfa618afba7571be65a6b2f80
Justin Rockwood <justinrockwood@hotmail.com>
2018-05-11 00:24:50 -0700

    refactor: add TemporaryVariableAllocator class

---
+++ src/Desalt.Core/Translation/TemporaryVariableAllocator.cs
@@ -0,0 +1,80 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="TemporaryVariableAllocator.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.Core.Translation
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+
+    /// <summary>
+    /// Responsible for allocating and keeping track of temporary variables.
+    /// </summary>
+    internal class TemporaryVariableAllocator
+    {
+        //// ===========================================================================================================
+        //// Member Variables
+        //// ===========================================================================================================
+
+        private readonly Dictionary<string, SortedSet<int>> _reserveSets = new Dictionary<string, SortedSet<int>>();
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        public string Reserve(string prefix)
+        {
+            int reservationNumber = 1;
+
+            // find the next number to use
+            if (_reserveSets.TryGetValue(prefix, out SortedSet<int> reserveSet))
+            {
+                reservationNumber = reserveSet.Max + 1;
+            }
+            else
+            {
+                _reserveSets.Add(prefix, new SortedSet<int>());
+            }
+
+            // reserve the number
+            _reserveSets[prefix].Add(reservationNumber);
+
+            return prefix + reservationNumber;
+        }
+
+        public void Return(string variableName)
+        {
+            // find the last digit in the string
+            int lastIndex = variableName.Length - 1;
+            while (lastIndex >= 0 && char.IsNumber(variableName[lastIndex]))
+            {
+                lastIndex--;
+            }
+
+            lastIndex++;
+
+            int reservationNumber = int.Parse(
+                variableName.Substring(lastIndex),
+                NumberStyles.Integer,
+                CultureInfo.InvariantCulture);
+
+            string prefix = variableName.Substring(0, lastIndex);
+
+            if (!_reserveSets.TryGetValue(prefix, out SortedSet<int> reserveSet))
+            {
+                throw new InvalidOperationException($"Prefix {prefix} does not have any reservations");
+            }
+
+            if (!reserveSet.Contains(reservationNumber))
+            {
+                throw new InvalidOperationException($"Variable {variableName} is not in the reservation set");
+            }
+
+            reserveSet.Remove(reservationNumber);
+        }
+    }
+}
---
+++ test/Desalt.Core.Tests/Translation/TemporaryVariableAllocatorTests.cs
@@ -0,0 +1,53 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="TemporaryVariableAllocatorTests.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.Core.Tests.Translation
+{
+    using Desalt.Core.Translation;
+    using FluentAssertions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class TemporaryVariableAllocatorTests
+    {
+        [TestMethod]
+        public void Reserve_should_keep_incrementing_values()
+        {
+            var allocator = new TemporaryVariableAllocator();
+            allocator.Reserve("test").Should().Be("test1");
+            allocator.Reserve("test").Should().Be("test2");
+            allocator.Reserve("test").Should().Be("test3");
+        }
+
+        [TestMethod]
+        public void Reserve_should_keep_track_of_different_prefixes()
+        {
+            var allocator = new TemporaryVariableAllocator();
+            allocator.Reserve("a").Should().Be("a1");
+            allocator.Reserve("b").Should().Be("b1");
+            allocator.Reserve("a").Should().Be("a2");
+        }
+
+        [TestMethod]
+        public void Reserve_should_return_prefixes()
+        {
+            var allocator = new TemporaryVariableAllocator();
+            allocator.Reserve("a").Should().Be("a1");
+            allocator.Reserve("a").Should().Be("a2");
+            allocator.Reserve("a").Should().Be("a3");
+
+            allocator.Return("a2");
+
+            allocator.Reserve("a").Should().Be("a4");
+
+            allocator.Return("a4");
+            allocator.Return("a3");
+
+            allocator.Reserve("a").Should().Be("a2");
+        }
+    }
+}
