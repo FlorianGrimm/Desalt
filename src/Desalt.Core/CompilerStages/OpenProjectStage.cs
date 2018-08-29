60d0ec4fb93bcb66dfd0f99cc239c76a6ad0831f
Justin Rockwood <jrockwood@tableau.com>
2018-08-29 13:52:19 -0700

    refactor: capture ReflectionTypeLoadException for better error messages

--- src/Desalt.Core/CompilerStages/OpenProjectStage.cs
+++ src/Desalt.Core/CompilerStages/OpenProjectStage.cs
@@ -7,6 +7,11 @@
 
 namespace Desalt.Core.CompilerStages
 {
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using System.Text;
+    using System.Text.RegularExpressions;
     using System.Threading;
     using System.Threading.Tasks;
     using Desalt.Core.Pipeline;
@@ -18,16 +23,62 @@ namespace Desalt.Core.CompilerStages
     /// </summary>
     internal class OpenProjectStage : PipelineStage<CompilationRequest, Project>
     {
+        //// ===========================================================================================================
+        //// Member Variables
+        //// ===========================================================================================================
+
+        private static readonly Regex s_workspaceErrorMessagePattern = new Regex(
+            @"(?<id>[^:]*): (?<message>.*)",
+            RegexOptions.ExplicitCapture | RegexOptions.Singleline);
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
         public override async Task<IExtendedResult<Project>> ExecuteAsync(
             CompilationRequest input,
             CompilerOptions options,
             CancellationToken cancellationToken = new CancellationToken())
         {
-            // try to open the project
-            MSBuildWorkspace workspace = MSBuildWorkspace.Create();
-            Project project = await workspace.OpenProjectAsync(input.ProjectFilePath, cancellationToken);
+            try
+            {
+                // try to open the project
+                var workspace = MSBuildWorkspace.Create();
+                Project project = await workspace.OpenProjectAsync(input.ProjectFilePath, cancellationToken);
 
-            return new ExtendedResult<Project>(project);
+                var diagnostics = workspace.Diagnostics.Select(WorkspaceDiagnosticToDiagnostic);
+                return new ExtendedResult<Project>(project, diagnostics);
+            }
+            catch (ReflectionTypeLoadException e)
+            {
+                string subMessages = e.LoaderExceptions.Aggregate(
+                    new StringBuilder().AppendLine().AppendLine("LoaderExceptions:"),
+                    (builder, ex) => builder.AppendLine(ex.Message),
+                    builder => builder.ToString());
+                throw new Exception($"{e.Message}{subMessages}");
+            }
+        }
+
+        private static Diagnostic WorkspaceDiagnosticToDiagnostic(WorkspaceDiagnostic workspaceDiagnostic)
+        {
+            // parse the message to get the id and the message parts
+            Match match = s_workspaceErrorMessagePattern.Match(workspaceDiagnostic.Message);
+            string id = match.Success ? match.Groups["id"].Value : "MSBuildWorkspace001";
+            string message = match.Success ? match.Groups["message"].Value : workspaceDiagnostic.Message;
+
+            DiagnosticSeverity severity = workspaceDiagnostic.Kind == WorkspaceDiagnosticKind.Failure
+                ? DiagnosticSeverity.Error
+                : DiagnosticSeverity.Warning;
+            int warningLevel = severity == DiagnosticSeverity.Error ? 0 : (int)WarningLevel.Important;
+
+            return Diagnostic.Create(
+                id,
+                category: "Workspace",
+                message: message,
+                severity: severity,
+                defaultSeverity: severity,
+                isEnabledByDefault: true,
+                warningLevel: warningLevel);
         }
     }
 }
