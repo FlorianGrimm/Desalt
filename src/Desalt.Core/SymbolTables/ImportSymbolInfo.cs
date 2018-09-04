62a15d2f6e82dbf3c47d26435ac516f811fb586a
Justin Rockwood <justinrockwood@hotmail.com>
2018-09-03 17:38:12 -0700

    refactor: move ImportSymbolInfo into its own file

---
+++ src/Desalt.Core/SymbolTables/ImportSymbolInfo.cs
@@ -0,0 +1,51 @@
+// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="ImportSymbolInfo.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.Core.SymbolTables
+{
+    /// <summary>
+    /// Represents an imported symbol that is contained in an <see cref="ImportSymbolTable"/>.
+    /// </summary>
+    internal class ImportSymbolInfo
+    {
+        //// ===========================================================================================================
+        //// Constructors
+        //// ===========================================================================================================
+
+        private ImportSymbolInfo(string relativeTypeScriptFilePathOrModuleName, bool isInternalReference)
+        {
+            RelativeTypeScriptFilePathOrModuleName = relativeTypeScriptFilePathOrModuleName;
+            IsInternalReference = isInternalReference;
+        }
+
+        //// ===========================================================================================================
+        //// Properties
+        //// ===========================================================================================================
+
+        public string RelativeTypeScriptFilePathOrModuleName { get; }
+
+        /// <summary>
+        /// Returns a value indicating whether this is an internal reference, meaning that the type
+        /// is defined within this project. An external reference is something from another assembly.
+        /// </summary>
+        public bool IsInternalReference { get; }
+
+        //// ===========================================================================================================
+        //// Methods
+        //// ===========================================================================================================
+
+        public static ImportSymbolInfo CreateInternalReference(string typeScriptFilePath)
+        {
+            return new ImportSymbolInfo(typeScriptFilePath, isInternalReference: true);
+        }
+
+        public static ImportSymbolInfo CreateExternalReference(string moduleName)
+        {
+            return new ImportSymbolInfo(moduleName, isInternalReference: false);
+        }
+    }
+}
--- src/Desalt.Core/SymbolTables/ImportSymbolTable.cs
+++ src/Desalt.Core/SymbolTables/ImportSymbolTable.cs
@@ -1,4 +1,4 @@
-﻿// ---------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------------------
 // <copyright file="ImportSymbolTable.cs" company="Justin Rockwood">
 //   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
 //   LICENSE.txt in the project root for license information.
@@ -93,48 +93,6 @@ namespace Desalt.Core.SymbolTables
             string moduleName = containingAssembly.Name;
             var symbolInfo = ImportSymbolInfo.CreateExternalReference(moduleName);
             return new KeyValuePair<ISymbol, ImportSymbolInfo>(symbol, symbolInfo);
-        }
-    }
-
-    /// <summary>
-    /// Represents an imported symbol that is contained in an <see cref="ImportSymbolTable"/>.
-    /// </summary>
-    internal class ImportSymbolInfo
-    {
-        //// ===========================================================================================================
-        //// Constructors
-        //// ===========================================================================================================
-
-        private ImportSymbolInfo(string relativeTypeScriptFilePathOrModuleName, bool isInternalReference)
-        {
-            RelativeTypeScriptFilePathOrModuleName = relativeTypeScriptFilePathOrModuleName;
-            IsInternalReference = isInternalReference;
-        }
-
-        //// ===========================================================================================================
-        //// Properties
-        //// ===========================================================================================================
-
-        public string RelativeTypeScriptFilePathOrModuleName { get; }
-
-        /// <summary>
-        /// Returns a value indicating whether this is an internal reference, meaning that the type
-        /// is defined within this project. An external reference is something from another assembly.
-        /// </summary>
-        public bool IsInternalReference { get; }
-
-        //// ===========================================================================================================
-        //// Methods
-        //// ===========================================================================================================
-
-        public static ImportSymbolInfo CreateInternalReference(string typeScriptFilePath)
-        {
-            return new ImportSymbolInfo(typeScriptFilePath, isInternalReference: true);
-        }
-
-        public static ImportSymbolInfo CreateExternalReference(string moduleName)
-        {
-            return new ImportSymbolInfo(moduleName, isInternalReference: false);
         }
     }
 }
