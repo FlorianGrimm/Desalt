62e35fb3aaa0715767c515d0cfb860f9680f3f12
Justin Rockwood <justinrockwood@hotmail.com>
2020-03-10 19:29:13 -0700

    Replace MSBuildWorkspace with Buildalyzer

--- src/Desalt.Core/CompilerStages/OpenProjectStage.cs
+++ src/Desalt.Core/CompilerStages/OpenProjectStage.cs
@@ -7,31 +7,18 @@
 
 namespace Desalt.Core.CompilerStages
 {
-    using System;
-    using System.Linq;
-    using System.Reflection;
-    using System.Text;
-    using System.Text.RegularExpressions;
     using System.Threading;
     using System.Threading.Tasks;
+    using Buildalyzer;
+    using Buildalyzer.Workspaces;
     using Desalt.Core.Pipeline;
-    using Microsoft.Build.Locator;
     using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.MSBuild;
 
     /// <summary>
     /// Pipeline stage that opens a .csproj file using Roslyn.
     /// </summary>
     internal class OpenProjectStage : PipelineStage<CompilationRequest, Project>
     {
-        //// ===========================================================================================================
-        //// Member Variables
-        //// ===========================================================================================================
-
-        private static readonly Regex s_workspaceErrorMessagePattern = new Regex(
-            @"(?<id>[^:]*): (?<message>.*)",
-            RegexOptions.ExplicitCapture | RegexOptions.Singleline);
-
         //// ===========================================================================================================
         //// Methods
         //// ===========================================================================================================
@@ -41,74 +28,20 @@ namespace Desalt.Core.CompilerStages
             CompilerOptions options,
             CancellationToken cancellationToken = new CancellationToken())
         {
-            try
-            {
-                // register the MSBuild locator so that it can find the right assemblies for MSBuild (see
-                // https://github.com/Microsoft/msbuild/issues/1889 and
-                // https://docs.microsoft.com/en-us/visualstudio/msbuild/updating-an-existing-application?view=vs-2017)
-                MSBuildLocator.RegisterDefaults();
-
-                // try to open the project
-                var workspace = MSBuildWorkspace.Create();
-                Project project = await workspace.OpenProjectAsync(
-                    input.ProjectFilePath,
-                    progress: null,
-                    cancellationToken: cancellationToken);
-
-                var diagnostics = workspace.Diagnostics.Where(IsValidError).Select(WorkspaceDiagnosticToDiagnostic);
-                return new ExtendedResult<Project>(project, diagnostics);
-            }
-            catch (ReflectionTypeLoadException e)
-            {
-                string subMessages = e.LoaderExceptions.Aggregate(
-                    new StringBuilder().AppendLine().AppendLine("LoaderExceptions:"),
-                    (builder, ex) => builder.AppendLine(ex.Message),
-                    builder => builder.ToString());
-                throw new Exception($"{e.Message}{subMessages}");
-            }
+            return await Task.Run(() => OpenProject(input.ProjectFilePath), cancellationToken);
         }
 
-        /// <summary>
-        /// Determines if the specified <see cref="WorkspaceDiagnostic"/> is "valid", meaning that
-        /// it's not one of the messages that we want to ignore.
-        /// </summary>
-        /// <param name="diagnostic">The <see cref="WorkspaceDiagnostic"/> to check.</param>
-        /// <returns>True if the error is "valid" and should be reported; otherwise, false.</returns>
-        private static bool IsValidError(WorkspaceDiagnostic diagnostic)
+        private static IExtendedResult<Project> OpenProject(string projectFilePath)
         {
-            // This error occurs because of a dynamic binding issue. We don't actually need to run
-            // the SCTask anyway, so just ignore it.
-            return !diagnostic.Message.Contains("The \"SCTask\" task failed unexpectedly");
-        }
+            // try to open the project
+            var manager = new AnalyzerManager();
+            ProjectAnalyzer projectAnalyzer = manager.GetProject(projectFilePath);
+            AdhocWorkspace workspace = projectAnalyzer.GetWorkspace();
 
-        /// <summary>
-        /// Converts a <see cref="WorkspaceDiagnostic"/> to a <see cref="Diagnostic"/> to use in the
-        /// compiler pipeline.
-        /// </summary>
-        /// <param name="workspaceDiagnostic">
-        /// A diagnostic issued when opening or compiling the project.
-        /// </param>
-        /// <returns>A compatible <see cref="Diagnostic"/> for use in the compiler pipeline.</returns>
-        private static Diagnostic WorkspaceDiagnosticToDiagnostic(WorkspaceDiagnostic workspaceDiagnostic)
-        {
-            // parse the message to get the id and the message parts
-            Match match = s_workspaceErrorMessagePattern.Match(workspaceDiagnostic.Message);
-            string id = match.Success ? match.Groups["id"].Value : "MSBuildWorkspace001";
-            string message = match.Success ? match.Groups["message"].Value : workspaceDiagnostic.Message;
+            var projectId = ProjectId.CreateFromSerialized(projectAnalyzer.ProjectGuid);
+            Project project = workspace.CurrentSolution.GetProject(projectId);
 
-            DiagnosticSeverity severity = workspaceDiagnostic.Kind == WorkspaceDiagnosticKind.Failure
-                ? DiagnosticSeverity.Error
-                : DiagnosticSeverity.Warning;
-            int warningLevel = severity == DiagnosticSeverity.Error ? 0 : (int)WarningLevel.Important;
-
-            return Diagnostic.Create(
-                id,
-                category: "Workspace",
-                message: message,
-                severity: severity,
-                defaultSeverity: severity,
-                isEnabledByDefault: true,
-                warningLevel: warningLevel);
+            return new ExtendedResult<Project>(project);
         }
     }
 }
