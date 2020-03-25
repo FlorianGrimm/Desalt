91a663f80573dad61ab0f8dd93ddbec6c8787161
Justin Rockwood <justinrockwood@hotmail.com>
2020-03-24 21:39:15 -0700

    Add IFileContentFetcher for easy unit testing

---
+++ src/Desalt.ConsoleApp/OsFileContentFetcher.cs
@@ -0,0 +1,27 @@
+﻿// ---------------------------------------------------------------------------------------------------------------------
+// <copyright file="OsFileContentFetcher.cs" company="Justin Rockwood">
+//   Copyright (c) Justin Rockwood. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+//   LICENSE.txt in the project root for license information.
+// </copyright>
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace Desalt.ConsoleApp
+{
+    using System.IO;
+
+    /// <summary>
+    /// Implementation of <see cref="IFileContentFetcher"/> that uses the OS file system.
+    /// </summary>
+    internal sealed class OsFileContentFetcher : IFileContentFetcher
+    {
+        public string ReadAllText(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
+
+        public Stream OpenRead(string filePath)
+        {
+            return File.OpenRead(filePath);
+        }
+    }
+}
