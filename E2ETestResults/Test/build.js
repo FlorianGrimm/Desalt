eb2abc88ab4bb54aaf9aa8b343ed31f9f170b6f7
Justin Rockwood <justinrockwood@hotmail.com>
2020-03-29 14:01:20 -0700

    Add the ability to compile the test TypeScript code in the playground

--- E2ETestResults/Test/build.js
+++ E2ETestResults/Test/build.js
@@ -1,41 +1,41 @@
-const child_process = require("child_process");
-const path = require("path");
+const child_process = require('child_process');
+const path = require('path');
 
-const isDebug = process.argv.indexOf("--release") < 0;
-const compilerBinariesPath = path.resolve(__dirname, "..", "..", "test", "SaltarelleBinaries");
+const isDebug = process.argv.indexOf('--release') < 0;
+const compilerBinariesPath = path.resolve(__dirname, '..', '..', 'test', 'SaltarelleBinaries');
 
 function getSourceFiles() {
-  const relativeFileNames = ["Test.cs"];
+  const relativeFileNames = ['Test.cs'];
 
   return relativeFileNames.map(file => path.normalize(file));
 }
 
 function getReferenceAssemblies() {
   const assemblies = [
-    "mscorlib.dll",
-    "NativeJsTypeDefs.dll",
-    "jQuery.dll",
-    "Web.dll",
-    "TypeDefs.dll",
-    "Underscore.dll"
+    'mscorlib.dll',
+    'NativeJsTypeDefs.dll',
+    'jQuery.dll',
+    'Web.dll',
+    'TypeDefs.dll',
+    'Underscore.dll',
   ];
 
-  const runtimeAssemblyPath = path.resolve(compilerBinariesPath, "Runtime");
+  const runtimeAssemblyPath = path.resolve(compilerBinariesPath, 'Runtime');
   return assemblies.map(assembly => path.resolve(runtimeAssemblyPath, assembly));
 }
 
 function getCommand() {
-  const scexe = path.resolve(compilerBinariesPath, "Cli", "sc.dll");
-  const options = `${isDebug ? "-debug" : ""} -w:0 -outasm:Test.dll -outscript:Test.js`;
+  const scexe = path.resolve(compilerBinariesPath, 'Cli', 'sc.dll');
+  const options = `${isDebug ? '-debug' : ''} -w:0 -outasm:Test.dll -outscript:Test.js`;
   const refs = getReferenceAssemblies()
     .map(ref => `-r:${ref}`)
-    .join(" ");
-  const sourceFiles = getSourceFiles().join(" ");
+    .join(' ');
+  const sourceFiles = getSourceFiles().join(' ');
 
   return `dotnet ${scexe} ${options} ${refs} ${sourceFiles}`;
 }
 
-console.log("Running Saltarelle compiler...");
+console.log('Running Saltarelle compiler...');
 
 child_process.exec(getCommand(), { cwd: __dirname }, (error, stdout, stderr) => {
   if (error) {
@@ -45,3 +45,18 @@ child_process.exec(getCommand(), { cwd: __dirname }, (error, stdout, stderr) => 
   console.log(stdout);
   console.error(stderr);
 });
+
+console.log('Running TypeScript compiler...');
+
+child_process.exec(
+  `tsc --pretty --target ES2015 --module commonjs --lib es2015,dom --strict --alwaysStrict --noImplicitAny test.ts`,
+  { cwd: path.resolve(__dirname, 'ts') },
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+    }
+
+    console.log(stdout);
+    console.error(stderr);
+  },
+);
